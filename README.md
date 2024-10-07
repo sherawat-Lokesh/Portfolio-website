c0a522b743df2a71d37eaee1299901e0e7ec56b7
SHERAWAT LOKESH <107927380+sherawat-Lokesh@users.noreply.github.com>
2024-10-07 05:46:19 +0530

    inital commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# E-Commerce Website
+
+A Ecommerce Website made with React.js Framework.
+
+
+## Demo
+
+https://e-commerce-tawny-two.vercel.app/
+
+## Features
+
+- Easy to integrate with Backend
+- Fully Responsive
+
+
+## Screenshots
+
+![App Screenshot](https://i.ibb.co/fQ293tm/image.png)
+
+
+
+## Run Locally
+
+
+Go to the project directory
+
+```bash
+  cd React_E-Commerce
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+
+
+## Tech Stack
+
+* [React](https://reactjs.org/)
+* [Redux](https://redux.js.org/)
+* [Bootstrap](https://getbootstrap.com/)
+* [Fake Store API](https://fakestoreapi.com/)
+
+
+
+
+
+
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+          Shop our eCommerce website for unique clothing and jewelry! Discover handcrafted apparel and one-of-a-kind accessories that showcase individual style. Enjoy a hassle-free shopping experience with secure payments and fast delivery, all while supporting talented artisans. Find your perfect statement piece today!
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">MEN'S CLOTHING</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">WOMEN'S CLOTHING</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">JEWELERY</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">ELECTRONICS</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
