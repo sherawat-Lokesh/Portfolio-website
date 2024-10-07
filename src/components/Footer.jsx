6debd5611e902ea2cff4b984104169ca705f76f1
SHERAWAT LOKESH <107927380+sherawat-Lokesh@users.noreply.github.com>
2024-10-07 06:17:21 +0530

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Lokesh Kumar</a>
+              <a  href="https://e-commerce-original-wt89.vercel.app/" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Lokesh Kumar</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/sherawat-Lokesh" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
